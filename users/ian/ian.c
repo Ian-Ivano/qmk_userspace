dee506c4f813cbb1c77fe97fe3d2512c657cb446
Ian-Ivano <paulomkali.jnr@gmail.com>
2026-08-29 20:15:51 +0300

    added and defined COMBO_COUNT

--- users/ian/ian.c
+++ users/ian/ian.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM underscore_combo[] = {KC_F, KC_J, COMBO_END};
 const uint16_t PROGMEM capsword_combo[] = {KC_U, KC_R, COMBO_END};
-combo_t key_combos[] = {
+combo_t key_combos[COMBO_COUNT] = {
     COMBO(underscore_combo, KC_UNDS),
     COMBO(capsword_combo, CW_TOGG),
 };
