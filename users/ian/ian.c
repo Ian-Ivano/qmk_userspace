0c86dbfe84f700b1e4c4217401fbe42ce6a2f9e4
Ian-Ivano <paulomkali.jnr@gmail.com>
2026-08-29 22:09:14 +0300

    deletede user folder

--- users/ian/ian.c
+++
@@ -1,8 +0,0 @@
-#include "quantum.h"
-
-const uint16_t PROGMEM underscore_combo[] = {KC_F, KC_J, COMBO_END};
-const uint16_t PROGMEM capsword_combo[] = {KC_U, KC_R, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {
-    COMBO(underscore_combo, KC_UNDS),
-    COMBO(capsword_combo, CW_TOGG),
-};
